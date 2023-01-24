b5704ae703a6632cdf76080a09669ea1d35ff6fb
Jefferson <jefficostau@gmail.com>
2023-01-24 10:20:31 -0300

    task of day 1

---
+++ Day1/printing.py
@@ -0,0 +1,42 @@
+#Write your code below this line 
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Writing a print statement with multiple linesSS
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " " + "John")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#input function
+print("Hello" + input("What is your name?"))
+
+#Exercise 3 - Input Function 
+print(len(input("What is your name? ")))
+
+#Write a program that switches the values stored in the variables a and b.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b 
+b = c 
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
